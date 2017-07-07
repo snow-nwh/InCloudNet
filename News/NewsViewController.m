4695ce9d273ec9f4b64550ed134d068e5f44ffa2
snow-nwh <704229196@qq.com>
2017-07-08 00:28:18 +0800

    1.5 improve the animation

--- News/NewsViewController.m
+++ News/NewsViewController.m
@@ -25,18 +25,45 @@
 }
 - (void)creatPointArray {
     NSMutableArray *array = [NSMutableArray array];
-    
+    CGPoint originPoint = CGPointMake(50, 400);
+    CGPoint oppositePoint = CGPointMake(350, 100);
     for (NSInteger i = 0; i < 10; i++) {
-        CGFloat x = i*30 +10;
-        CGFloat y = arc4random()%300 +100;
+        CGFloat x = i*(oppositePoint.x-originPoint.x)/10 +originPoint.x;
+        CGFloat y = arc4random()%(int32_t)(originPoint.y-oppositePoint.y) +oppositePoint.y;
         CGPoint point = CGPointMake(x, y);
         NSString *str = NSStringFromCGPoint(point);
         [array addObject:str];
     }
-    [self drawLineWithArray:array];
+    [self drawLineWithArray:array
+                originPoint:originPoint
+              oppositePoint:oppositePoint];
 }
 
-- (void)drawLineWithArray:(NSArray *)array {
+- (void)drawLineWithArray:(NSArray *)array
+              originPoint:(CGPoint)originPoint
+            oppositePoint:(CGPoint)oppositePoint{
+    UIBezierPath *yPath = [UIBezierPath bezierPath];
+    CAShapeLayer *yLayer = [CAShapeLayer layer];
+    [yPath moveToPoint:originPoint];
+    [yPath addLineToPoint:CGPointMake(originPoint.x, oppositePoint.y)];
+    [yPath stroke];
+    yLayer.path = yPath.CGPath;
+    yLayer.strokeColor = [UIColor blueColor].CGColor;
+    yLayer.lineWidth = 2.0f;
+    yLayer.lineDashPattern = @[@(10),@(10)];
+    [self.view.layer addSublayer:yLayer];
+    
+    UIBezierPath *xPath = [UIBezierPath bezierPath];
+    CAShapeLayer *xLayer = [CAShapeLayer layer];
+    [xPath moveToPoint:originPoint];
+    [xPath addLineToPoint:CGPointMake(oppositePoint.x, originPoint.y)];
+    [xPath stroke];
+    xLayer.path = xPath.CGPath;
+    xLayer.strokeColor = [UIColor blueColor].CGColor;
+    xLayer.lineWidth = 2.0f;
+    xLayer.lineDashPattern = @[@(10),@(10)];
+    [self.view.layer addSublayer:xLayer];
+    
     UIBezierPath *bezier = [UIBezierPath bezierPath];
     CAShapeLayer *layer = [CAShapeLayer layer];
 
@@ -57,9 +84,14 @@
         [button setBackgroundColor:[UIColor redColor]];
         [layer addSublayer:button.layer];
         
-        CABasicAnimation *animation = [CABasicAnimation animationWithKeyPath:@"position.x"];
+        CABasicAnimation *animation = [CABasicAnimation animationWithKeyPath:@"position"];//keyPath直接给position不用.x或.y
         animation.duration = i*0.1f;
-        animation.fromValue = [NSValue valueWithCGPoint:CGPointMake(0, point.y)];
+        //top
+        animation.fromValue = [NSValue valueWithCGPoint:CGPointMake(point.x, 0)];
+        //left
+//        animation.fromValue = [NSValue valueWithCGPoint:CGPointMake(0, point.y)];
+        //bottom
+        animation.fromValue = [NSValue valueWithCGPoint:CGPointMake(point.x, originPoint.y)];
         animation.toValue = [NSValue valueWithCGPoint:point];
         
         [button.layer addAnimation:animation forKey:nil];
@@ -78,6 +110,7 @@
     animation.duration = 0.9f;
     animation.timingFunction = [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseInEaseOut];
     [layer addAnimation:animation forKey:nil];
+    
 }
 
 - (void)loadWebView {
@@ -91,7 +124,8 @@
 }
 
 - (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
-    for (CAShapeLayer *layer in self.view.layer.sublayers) {
+    for (NSInteger i = 0; i < self.view.layer.sublayers.count;) {
+        CALayer *layer = self.view.layer.sublayers[i];
         [layer removeFromSuperlayer];
     }
     [self creatPointArray];
